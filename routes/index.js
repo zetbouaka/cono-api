5a8ba68ac7c7a2c25930e5aeede14a444e5bd291
진대윤 <zetbouaka@gmail.com>
2022-03-01 07:33:40 +0000

    add simple test

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,13 @@
 const { Router } = require('express');
 const { SuccessResponseObject } = require('../common/http');
 const demo = require('./demo.route');
+const user = require('./user');
 
 const r = Router();
 
 r.use('/demo', demo);
+
+r.use('/user/', user);
 
 r.get('/', (req, res) => res.json(new SuccessResponseObject('express vercel boiler plate')));
 
---
+++ routes/user.js
@@ -0,0 +1,12 @@
+const { Router } = require('express');
+const { SuccessResponseObject } = require('../common/http');
+
+const r = Router();
+
+r.get('/', (req, res) => res.json(new SuccessResponseObject('demo path live 🚀')));
+
+app.get('/:id', function(req , res){
+    res.render('article' + req.params.id);
+});
+
+module.exports = r;

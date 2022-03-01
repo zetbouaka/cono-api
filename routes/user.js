f9cbfa35a82d1c915dff2134c3c27c3b19a579dd
진대윤 <zetbouaka@gmail.com>
2022-03-01 07:59:20 +0000

    user add

--- routes/user.js
+++ routes/user.js
@@ -6,10 +6,19 @@ const r = Router();
 r.get('/', (req, res) => res.json(new SuccessResponseObject('demo path live 🚀')));
 
 r.get('/:id', function(req , res){
-    const songA = {
-        SongTitleInJapanese: '僕の戦争',
-        SongTitleInKorean: '나의 전쟁',
-        SongNumberInTJ: '68371'
+    if (res.params.id == 'KAMIZO'){
+        const songA = {
+            SongTitleInJapanese: '希望',
+            SongTitleInKorean: '희망',
+            SongNumberInTJ: '-'
+        };
+    }else
+    {
+        const songA = {
+            SongTitleInJapanese: '僕の戦争',
+            SongTitleInKorean: '나의 전쟁',
+            SongNumberInTJ: '68371'
+        };
     }
     const result = {
         userid: req.params.id,

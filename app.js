cc0f6fda3605eb077a088c9952031b310747d660
진대윤 <zetbouaka@gmail.com>
2022-03-05 08:43:39 +0000

    fix CORS problem

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require('express');
 const helmet = require('helmet');
 const { ErrorResponseObject } = require('./common/http');
 const routes = require('./routes');
+const cors = require('cors');
 
 const app = express();
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ extended: true, limit: '50mb' }));
 app.use(helmet());
+app.use(cors());
 app.use('/', routes);
 
 // default catch all handler
